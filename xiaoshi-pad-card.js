e1c926be4b7e4b25b32ef6e0db395e31b9e1ac8a
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2025-11-23 22:49:10 +0800

    Update xiaoshi-pad-card.js

--- xiaoshi-pad-card.js
+++ xiaoshi-pad-card.js
@@ -1,8 +1,9 @@
-console.info("%c 消逝卡-平板端 \n%c      v 0.0.0 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
+console.info("%c 消逝卡-平板端 \n%c      v 0.0.1 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
 
 const loadCards = async () => {
     await import('./xiaoshi-pad-grid-card.js');
-    await import('./xiaoshi-pad-slider-card.js');
+    await import('./xiaoshi-device-update-card.js');
+    await import('./xiaoshi-device-offline-card.js');
     
     window.customCards = window.customCards || [];
     window.customCards.push(...cardConfigs);
@@ -15,9 +16,16 @@ const cardConfigs = [
     description: '温度分布、湿度分布'
   },
   {
-    type: 'xiaoshi-pad-slider-card',
-    name: '消逝卡(平板端)-进度条',
-    description: '进度条'
+    type: 'xiaoshi-update-card',
+    name: '消逝卡HA更新监控卡片',
+    description: '显示需要更新的组件和版本',
+    preview: true
+  },
+  {
+    type: 'xiaoshi-offline-card',
+    name: '消逝卡HA离线设备卡片',
+    description: '显示所有离线的设备和实体',
+    preview: true
   }
 ];
 
