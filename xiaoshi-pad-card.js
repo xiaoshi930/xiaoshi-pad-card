2aeca95db9a451ca0e4d710d3623f3096fee1739
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2026-01-01 15:45:28 +0800

    Update xiaoshi-pad-card.js

--- xiaoshi-pad-card.js
+++ xiaoshi-pad-card.js
@@ -1,4 +1,4 @@
-console.info("%c 消逝卡-平板端 \n%c      v 0.2.5 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
+console.info("%c 消逝卡-平板端 \n%c      v 0.2.6 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
 
 const loadCards = () => {
     import('./xiaoshi-pad-grid-card.js');
