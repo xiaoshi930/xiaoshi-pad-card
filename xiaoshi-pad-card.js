2f8c0c2beb4e7376362afe0424f6527d190a6a98
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2026-01-25 16:17:34 +0800

    Update xiaoshi-pad-card.js

--- xiaoshi-pad-card.js
+++ xiaoshi-pad-card.js
@@ -1,6 +1,7 @@
-console.info("%c 消逝卡-平板端 \n%c      v 1.0.0 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
+console.info("%c 消逝卡-平板端 \n%c      v 1.0.1 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
 
 const loadCards = () => {
+    import('./xiaoshi-pad-climate-card.js');
     import('./xiaoshi-pad-grid-card.js');
     
     window.customCards = window.customCards || [];
@@ -12,6 +13,12 @@ const cardConfigs = [
     type: 'xiaoshi-pad-grid-card',
     name: '消逝卡(平板端)-分布卡',
     description: '温度分布、湿度分布'
+  },
+  {
+    type: 'xiaoshi-pad-climate-card',
+    name: '消逝卡(平板端)-空调/水暖毯/热水器卡',
+    description: '平板端空调/水暖毯/热水器卡',
+    preview: true
   }
 ];
 
