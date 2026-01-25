e12f60b2840fcfc7ddebb5181a6be267d09491bc
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2026-01-25 16:46:02 +0800

    Update xiaoshi-pad-card.js

--- xiaoshi-pad-card.js
+++ xiaoshi-pad-card.js
@@ -1,4 +1,4 @@
-console.info("%c 消逝卡-平板端 \n%c      v 1.0.2 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
+console.info("%c 消逝卡-平板端 \n%c      v 1.0.3 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
 
 const loadCards = () => {
     import('./xiaoshi-pad-climate-card.js');
